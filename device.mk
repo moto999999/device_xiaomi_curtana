f1fefca7f779a9643aefb6046f7cea348d292ce7
Juan Ramírez <juan1999l@gmail.com>
2020-05-13 22:23:24 +0200

    curtana: overlay: OwlsNest: Set maintainer name

--- device.mk
+++ device.mk
@@ -17,7 +17,8 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-aosip
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
